2f74b7f5e9ca8ae3b206bdaefd30262b0899e904
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-24 10:16:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,21 @@ class Song
   attr_accessor: :name, :artist, :genre 
   
   @@count = 0 
+  @@genres = []
   
   def initialize
     @@count += 1 
+    @@genres += 1 
   end 
   
   def .count 
     puts @@count 
   end 
+  
+  def .genres 
+    
+    
+  end 
+  
+end 
+  
